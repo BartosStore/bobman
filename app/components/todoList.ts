827577c2fbd283f12017b7ada83ec05e05541bed
Miroslav Bartoš <mirek-b@seznam.cz>
2019-04-27 19:07:48 +0200

    Dump all tasks into todo list

--- app/components/todoList.ts
+++ app/components/todoList.ts
@@ -26,7 +26,21 @@ export const create = b.createComponent<IContext>({
     render(ctx: IContext, me: b.IBobrilNode) {
         me.children = m.Paper({
             children: [
-                b.styledDiv('Todo List')
+                b.styledDiv('Todo List', labelStyle),
+                b.styledDiv(
+                    ctx.tasks.map((task, i) =>
+                    b.styledDiv(
+                        m.Checkbox({
+                            children: getTaskView(task),
+                            checked: task.isDone,
+                            action: () => {
+                                task.isDone = !task.isDone;
+                                b.invalidate(ctx);
+                            }
+                        })
+                    )
+                    )
+                )
             ]
         });
     }
@@ -35,3 +49,21 @@ export const create = b.createComponent<IContext>({
 function createTask(description: string, isDone: boolean) {
     return {description, isDone}
 }
+
+function getTaskView(task: ITask): b.IBobrilChildren {
+    return b.styledDiv(
+        task.description,
+        task.isDone && {textDecoration: 'line-through'}
+    );
+}
+
+const padding = 16;
+const taskListHeight = 120;
+const taskListStyle = b.styleDef({
+    paddingTop: padding,
+    paddingBottom: padding,
+    height: taskListHeight,
+    overflow: 'auto'
+});
+const labelStyle = b.styleDef({fontSize: 18, textAlign: 'left'});
+const taskStyle = b.styleDef({textAlign: 'left'});
