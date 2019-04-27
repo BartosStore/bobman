62ff6c7df086be8e7c99a82a6d67d092766d2e0a
Miroslav Bartoš <mirek-b@seznam.cz>
2019-04-27 17:54:24 +0200

    Extend simple page

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import * as b from 'bobril';
 
 b.init(() => {
-    return {tag: 'h1', children: 'Hello world!'};
+    return {tag: 'div', children: [
+        {tag: 'h1', children: 'Bobman'},
+        {tag: 'p', children: 'This is simple javascript app.'},
+        {tag: 'p', children: 'Lets play...'}
+    ]};
 })
