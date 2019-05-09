472486bde8d81fbf0ff6114def7b8d9e6946971d
Miroslav Bartoš <mirek-b@seznam.cz>
2019-05-09 10:18:04 +0200

    Create ctx class for page

--- app/app.ts
+++ app/app.ts
@@ -2,13 +2,13 @@ import * as b from 'bobril';
 import { pageMain } from './pageMain';
 import { pageCounter } from './pageCounter';
 import { PageTodoList } from './pageTodoList';
-import { PageSimple } from './pageSimple';
+import { RestCaller } from './pageSimple';
 
 b.routes(
     b.route({handler: pageMain}, [
         b.route({url: "/counter", name: "counter", handler: pageCounter}),
         b.route({url: "/todo-list", name: "todo_list", handler: PageTodoList}),
-        b.route({url: "/simple", name: "simple", handler: PageSimple}),
+        b.route({url: "/simple", name: "simple", handler: RestCaller}),
         b.routeDefault({handler: pageCounter})
     ])
 );
--- app/pageSimple.ts
+++ app/pageSimple.ts
@@ -1,11 +1,29 @@
 import * as b from "bobril";
 import { Button } from "./components/button";
 
-export const PageSimple = b.createComponent({
-    render(_ctx: b.IBobrilCtx, me: b.IBobrilNode): void {
+interface IData {}
+
+class RestCallerCtxStore extends b.BobrilCtx<IData> {
+    private _pageText: string = "Simple empty page.";
+    private _url: string = "https://api.github.com/repos/BartosStore/HeatingProject/contributors";
+
+    constructor(data: IData) {
+        super(data);
+    }
+
+    pageText = (): string => {
+        return this._pageText;
+    }
+}
+
+export const RestCaller = b.createComponent({
+    id: "rest-caller",
+    ctxClass: RestCallerCtxStore,
+    render(ctx: RestCallerCtxStore, me: b.IBobrilNode): void {
+        const text = ctx.pageText();
         me.children = [
             {tag: "h1", children: "Simple Page"},
-            {tag: "p", children: "Simple empty page."},
+            {tag: "p", children: "" },
             Button({
                 caption: "Next page",
                 onHit: () => {
